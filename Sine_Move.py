0044a7feb66afb1067ce2b1d2b50f898d9739da4
Mark <markind@hotmail.com>
2019-08-05 16:04:09 -0700

    Fixes sign_changer bugs to Sine_Move - now works in S1

--- Sine_Move.py
+++ Sine_Move.py
@@ -1,10 +1,9 @@
 # from time import sleep
 from math import sin, cos, radians, pi, fabs
-Tm = 5
-Vd = 5
+Tm = 0.2
+Vd = 25
 sign_changer = -1.0
 # time.sleep(Tm)
-
 
 def mechanum(Vd, Vo, T0):
 
@@ -15,13 +14,13 @@ def mechanum(Vd, Vo, T0):
     return [V0, V1, V2, V3]
 # Demo - prints a sine curve with * and cos curve with #
 # Modified to print something readable on the S1 Console
-for n in range(28, 90):
-    circle_1 = (1 * (1 + sin(radians(n * 10)))) * sign_changer
+for n in range(28, 130):
+    circle_1 = (0.3 * (1 + sin(radians(n * 10)))) * sign_changer
     print(circle_1)
     if fabs(circle_1) < .01:
         sign_changer = sign_changer * -1.0
         print(sign_changer)
-    V = mechanum(5,circle_1,0)
-    # chassis_ctrl.set_wheel_speed(V1, V2, V3, V4)
+    V = mechanum(Vd,circle_1,0)
+    chassis_ctrl.set_wheel_speed(V[0], V[1], V[2], V[3])
     print(V)
-    # time.sleep(Tm)
+    time.sleep(Tm)
