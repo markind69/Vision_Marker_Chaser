7ac37edce5a881703b67659b23f51d588f22e1ac
Mark <markind@hotmail.com>
2019-08-04 19:47:50 -0700

    Added mecanum function to Robot_Class.py

--- Robot_Class.py
+++ Robot_Class.py
@@ -1,4 +1,10 @@
+from math import sin, cos, radians, pi
+
 class Robot:
+    Vd = 5
+    T0 = 0
+    Vo = 0
+    Tm = 5
 
     def __init__(self, name, color, armor, weapon, speed):
         self.name = name
@@ -12,6 +18,14 @@ class Robot:
                 f' {self.armor} and my Program\n is to destroy all humans with '
                 f'my {self.weapon}\n at {self.speed} meters per second.')
 
+    def mechanum(self, Vd, Vo, T0):
+        V0 = Vd * (sin(radians(T0) + pi / 4) + Vo)
+        V1 = Vd * (cos(radians(T0) + pi / 4) - Vo)
+        V2 = Vd * (cos(radians(T0) + pi / 4) + Vo)
+        V3 = Vd * (sin(radians(T0) + pi / 4) - Vo)
+        return [V0, V1, V2, V3]
+
+
 Robot_1 = Robot('Blast-Star', 'Red', 'Titanium', 'Ion Cannon', 20)
 Robot_2 = Robot('Eye-Scythe', 'Silver', 'Aluminum', 'Mass Driver', 30)
 Robot_3 = Robot('Vader', 'Black', 'Steel', 'Plasma Beam', 30)
@@ -22,3 +36,11 @@ print(Robot_2.program())
 print(Robot_2.armor)
 print(Robot_3.name)
 print(Robot_1.weapon)
+
+V = Robot_2.mechanum(5,2,23)
+print(V)
+V = Robot_3.mechanum(7,1,13)
+print(V)
+# V = mechanum(Vd, Vo, T0)
+# chassis_ctrl.set_wheel_speed(V[0], V[1], V[2], V[3])
+# time.sleep(Tm)
