538069b8c9e48780bf822c3b9c167518b1844e8e
Mark <markind@hotmail.com>
2019-08-05 15:39:48 -0700

    Added sign_changer to Sine_Move

--- Sine_Move.py
+++ Sine_Move.py
@@ -1,7 +1,8 @@
 # from time import sleep
-from math import sin, cos, radians, pi
+from math import sin, cos, radians, pi, fabs
 Tm = 5
 Vd = 5
+sign_changer = -1.0
 # time.sleep(Tm)
 
 
@@ -15,8 +16,11 @@ def mechanum(Vd, Vo, T0):
 # Demo - prints a sine curve with * and cos curve with #
 # Modified to print something readable on the S1 Console
 for n in range(28, 90):
-    circle_1 = 1 * (1 + sin(radians(n * 10)))
+    circle_1 = (1 * (1 + sin(radians(n * 10)))) * sign_changer
     print(circle_1)
+    if fabs(circle_1) < .01:
+        sign_changer = sign_changer * -1.0
+        print(sign_changer)
     V = mechanum(5,circle_1,0)
     # chassis_ctrl.set_wheel_speed(V1, V2, V3, V4)
     print(V)
