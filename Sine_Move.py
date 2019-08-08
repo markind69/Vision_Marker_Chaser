c9f2368afd7f1e3ef20a333cfde9741d8781c8c0
Mark <markind@hotmail.com>
2019-08-08 16:08:58 -0700

    Adds sine function as input for Speed variation

--- Sine_Move.py
+++ Sine_Move.py
@@ -1,14 +1,17 @@
 from time import sleep
-from math import sin, cos, radians, pi, fabs
+from math import sin, cos, radians, pi, fabs, acos, asin
 T0 = 0
 Tm = 0.2
 Vd = 25
+vdmax = 25
 sign_changer1 = -1.0
 sign_changer2 = -1.0
-start1 = 30
+sign_changer3 = -1.0
+start1 = 63
 stop1 = 230
 trfact = 0.3   # Turn Radius Factor
-ltfact = 10     # Lateral Translation Factor
+ltfact = 10    # Lateral Translation Factor
+vdfact = 1     # Speed Factor
 
 
 def mecanum(Vd, Vo, T0):
@@ -23,13 +26,17 @@ def mecanum(Vd, Vo, T0):
 for n in range(start1, stop1):
     Vo = (trfact * (1 + sin(radians(n * 10)))) * sign_changer1
     T0 = (ltfact * (1 + cos(radians(n * 10)))) * sign_changer2
+    Vd = (vdfact * (1 + sin(radians(n * 10)))) * sign_changer3
     if fabs(Vo) < .01:
         sign_changer1 = sign_changer1 * -1.0
         print(sign_changer1)
     if fabs(T0) > 45:
         sign_changer2 = sign_changer2 * -1.0
         print(sign_changer2)
-    print(Vo, T0)
+    if fabs(Vd) > vdmax:
+        sign_changer3 = sign_changer3 * -1.0
+        print(sign_changer3)
+    print(Vd, Vo, T0, n)
     V = mecanum(Vd, Vo, T0)
     # chassis_ctrl.set_wheel_speed(V[0], V[1], V[2], V[3])
     print(V[0], V[1], V[2], V[3])
