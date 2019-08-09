9795c8b1a8c7ba1cd5db780054eff72c46ffb939
Mark <markind@hotmail.com>
2019-08-08 17:26:11 -0700

    Adds variables at top to control Speed variation

--- Sine_Move.py
+++ Sine_Move.py
@@ -2,17 +2,18 @@ from time import sleep
 from math import sin, cos, radians, pi, fabs, acos, asin
 T0 = 0
 Tm = 0.2
-Vd = 25
-vdmax = 25
+Vd = 40
+vdmax = 59.9
 sign_changer1 = -1.0
 sign_changer2 = -1.0
-sign_changer3 = -1.0
+sign_changer3 = 1.0
 start1 = 63
 stop1 = 230
-trfact = 0.3   # Turn Radius Factor
-ltfact = 10    # Lateral Translation Factor
-vdfact = 1     # Speed Factor
-
+trfact = 0.4   # Turn Radius Factor
+ltfact = 30    # Lateral Translation Factor
+vdfact = 30     # Speed Factor
+t0max = 60
+voff = 45
 
 def mecanum(Vd, Vo, T0):
 
@@ -26,11 +27,11 @@ def mecanum(Vd, Vo, T0):
 for n in range(start1, stop1):
     Vo = (trfact * (1 + sin(radians(n * 10)))) * sign_changer1
     T0 = (ltfact * (1 + cos(radians(n * 10)))) * sign_changer2
-    Vd = (vdfact * (1 + sin(radians(n * 10)))) * sign_changer3
+    Vd = (vdfact * (1 + sin(radians((n+voff) * 10)))) * sign_changer3
     if fabs(Vo) < .01:
         sign_changer1 = sign_changer1 * -1.0
         print(sign_changer1)
-    if fabs(T0) > 45:
+    if fabs(T0) > t0max:
         sign_changer2 = sign_changer2 * -1.0
         print(sign_changer2)
     if fabs(Vd) > vdmax:
