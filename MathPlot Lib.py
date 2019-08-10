0c665914bd5703e4293e00cff7169a04b0ebe922
Mark <markind@hotmail.com>
2019-08-10 00:02:31 -0700

    Added matplot lib practice file

--- MathPlot Lib.py
+++ MathPlot Lib.py
@@ -4,11 +4,18 @@ import numpy as np
 
 Fs = 8000
 f = 5
-sample = 8000
+print(type(f))
+sample = 100
 x = np.arange(sample)
 print(type(x))
-y = np.sin(2 * np.pi * f * x / Fs)
+# y = np.sin(2 * np.pi * f * x / Fs)
+y = (0.3 * (1 + np.sin(np.radians(x * 10))))
+t = (0.3 * (1 + np.cos(np.radians(x * 10))))
+print(type(y))
 plt.plot(x, y)
-plt.xlabel('sample(n)')
-plt.ylabel('voltage(V)')
+plt.plot(x, t)
+plt.xlabel('Counter in degrees (Y)')
+plt.ylabel('Vo(Turning Factor)')
+plt.tlabel('Lateral Translation Direction (in Degrees)')
+
 plt.show()
